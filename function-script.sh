20127b859716ec59917f216cf1092a308c394a9d
Santhosh <m.santhosh3k4@gmail.com>
2025-09-09 21:55:15 +0530

    shell script

--- function-script.sh
+++ function-script.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
-R=$(\e[31m)
-B=$(\e[32m)
-USER_ID=$(id -u)
 
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 
-if [ $USER_ID -ne 0 ]
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ $USERID -ne 0 ]
 then
-    echo -e $R "Not a root user"
-else
-    dnf install mysql -y 
+    echo "ERROR:: You must have sudo access to execute this script"
+    exit 1 #other than 0
 fi
 
-# if[ $? -eq 0 ]
-# then
-#     Validate $USER_ID
-#     dnf install git -y 
-#     echo -e $R "installed successfull"
-# else
-#     echo -e $B "Previous installation not done"
-# fi
+dnf list installed mysql
 
+if [ $? -ne 0 ]
+then # not installed
+    dnf install mysql -y
+    VALIDATE $? "Installing MySQL"
+else
+    echo -e "MySQL is already ... $Y INSTALLED"
+fi
+
+
+dnf list installed git
+
+if [ $? -ne 0 ]
+then
+    dnf install git -y
+    VALIDATE $? "Installing Git"
+else
+    echo -e "Git is already ... $Y INSTALLED"
+fi
