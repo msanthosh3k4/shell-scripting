72bfbe172a96c87cd44076613ff8241a405efbf8
Santhosh <m.santhosh3k4@gmail.com>
2025-09-09 22:27:25 +0530

    shell script

--- loop.sh
+++ loop.sh
@@ -1,33 +1,3 @@
-#!/bin/bash
-
-USERID=$(id -u)
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-        echo -e "$2 ... $R FAILURE"
-        exit 1
-    else
-        echo -e "$2 ... $G SUCCESS"
-    fi
-}
-
-for packeges in $@
-do
-    dnf list installed $package
-    if[ $? -ne 0 ]
-    then
-        dnf install $package
-        VALIDATE $? "Installing"
-    else 
-        echo "package is already installed"
-        exit 1 #other than 0
-    fi
-done
-
 
 #!/bin/bash
 
