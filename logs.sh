f6a1d4ec63396eba8fbd3811140d09d2eb5da56b
Santhosh <m.santhosh3k4@gmail.com>
2025-09-10 15:38:47 +0530

    shell script

--- logs.sh
+++ logs.sh
@@ -5,9 +5,11 @@ USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-Time_Stamp=$(date)
-logFile="shell-scripting/logs.txt"
 
+log-Folder="var/logs/shell-scriptinglogs"
+log-file=$(echo $0 | cut -d "." -f1)
+Time_Stamp=$(date +%y-%m-%d)
+log-file-name="log-Folder/log-file//log-file-name-$Time_Stamp.log"
 VALIDATE() {
     if [ $1 -ne 0 ]; then
         echo -e "$2 ... $R FAILURE"
@@ -18,17 +20,17 @@ VALIDATE() {
 }
 
 if [ "$USERID" -ne 0 ]; then
-    echo -e "${R}You must be root to run this script"
+    echo -e "${R}You must be root to run this script" &>>log-file-name
     exit 1
 fi
 
 for package in "$@"; do
-    dnf list installed "$package" 
+    dnf list installed "$package" &>>log-file-name
     if [ $? -ne 0 ]; then
         dnf install $package -y 
-        VALIDATE $? "Installing $package" &>>logFile
+        VALIDATE $? "Installing $package" &>>log-file-name 
     else 
-        echo -e "$Y$package is already installed:$Time_Stamp" &>>logFile
+        echo -e "$Y$package is already installed:$Time_Stamp" &>>log-file-name 
     fi
 done
 
