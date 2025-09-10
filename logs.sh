0a5aaa1bac4dae83451ecd417ff253d31b895447
Santhosh <m.santhosh3k4@gmail.com>
2025-09-10 15:15:11 +0530

    shell script

--- logs.sh
+++ logs.sh
@@ -1,3 +1,35 @@
-#!bin/bash
 
-echo "Hello"
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+Time_Stamp=$(date)
+
+VALIDATE() {
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS"
+    fi
+}
+
+if [ "$USERID" -ne 0 ]; then
+    echo -e "${R}You must be root to run this script"
+    exit 1
+fi
+
+for package in "$@"; do
+    dnf list installed "$package" 
+    if [ $? -ne 0 ]; then
+        dnf install $package -y 
+        VALIDATE $? "Installing $package"
+    else 
+        echo -e "$Y$package is already installed:$Time_Stamp"
+    fi
+done
+
+
+
