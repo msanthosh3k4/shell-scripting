e0a5a23dbc5405fae2ee9324844260b4602eff41
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-09-30 07:57:25 +0530

    delete.sh

--- zip_folder.sh
+++ zip_folder.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+
 source_Dir="/home/ec2-user/shell-logs"
 log_Store="/home/ec2-user/logged_logs/logs-$(date +'%Y-%m-%d_%H-%M-%S').log"
-
+DAYS=${1:-14}
 
 if [ -d "$source_Dir" ]; then
-    FILES_TO_Zip=$(find "$source_Dir" -name "*.log" -mtime +10)
-    if [ -n "$FILES_TO_Zip" ]
-    then   
-        echo "these are files to ZIP : $FILES_TO_Zip" &>>log_Store
-    else
-        echo "no files to zip" &>>log_Store
-    fi
+    FILES_TO_Zip=$(find "$source_Dir" -name "*.log" -mtime +"$DAYS")
+    
+    for file in $FILES_TO_Zip; do
+        echo "Found: $file" >> "$log_Store"
+    done
+else
+    echo "Directory not found: $source_Dir" >> "$log_Store"
 fi
