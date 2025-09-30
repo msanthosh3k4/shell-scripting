72e40ed44dc4db338f7e7da7010c953a8c445a8e
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-09-30 07:55:25 +0530

    delete.sh

--- zip_folder.sh
+++ zip_folder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 source_Dir="/home/ec2-user/shell-logs"
 log_Store="/home/ec2-user/logged_logs/logs-$(date +'%Y-%m-%d_%H-%M-%S').log"
-DAYS=${1:-14}
+
 
 if [ -d "$source_Dir" ]; then
-    FILES_TO_Zip=$(find "$source_Dir" -name "*.log" -mtime +"$DAYS")
+    FILES_TO_Zip=$(find "$source_Dir" -name "*.log" -mtime +10)
     if [ -n "$FILES_TO_Zip" ]
     then   
         echo "these are files to ZIP : $FILES_TO_Zip" &>>log_Store
