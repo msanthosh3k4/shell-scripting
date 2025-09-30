a7380c00609ab67f2d4cd3673eabd83f053d49e6
msanthosh3k4 <m.santhosh3k4@gmail.com>
2025-09-30 07:22:43 +0530

    delete.sh

--- deleting.sh
+++ deleting.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 source_Dir="/home/ec2-user/shell-logs"
+log_Store = "/home/ec2-user/logged_logs"
 
 if [ -d "$source_Dir" ]; 
 then
     FILES_TO_DELETE=$(find "$source_Dir" -name "*.log" -mtime +14)
     while read -r file; do
-        echo "$file"
+        echo "$file" &>>log_Store
     done <<< "$FILES_TO_DELETE"
 fi
